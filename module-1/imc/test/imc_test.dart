ed9f6b4740b3c476b7dd74b7ce164b2d07ff0966
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-02-07 17:30:25 -0300

    Added some functions to the life cycle da aplicação.

--- module-1/imc/test/imc_test.dart
+++ module-1/imc/test/imc_test.dart
@@ -3,13 +3,25 @@ import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  group('Testes de calculo IMC', () {
-    test('Deve efetuar o calculo do IMC', () {
-      final result = calcIMC(1.78, 64);
+  // Executa Antes de Realizar Algum Teste.
+  setUp(() {});
 
-      expect(result, equals(20.199469763918696));
-    });
+  // Executa depois de executar algum teste.
+  tearDown(() {});
 
+  // Ciclo de vida pra Switch inteira
+  setUpAll(() {});
+
+  // Ciclo de Vida pra Switch inteira
+  tearDownAll(() {});
+
+  test('Deve efetuar o calculo do IMC', () {
+    final result = calcIMC(1.78, 64);
+
+    expect(result, equals(20.199469763918696));
+  });
+
+  group('Exceções de Parâmetros', () {
     test('Deve lançar uma excessão se altura for menor que 1', () {
       expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
     });
