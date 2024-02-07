77c8273306ff3e7b8cdddaa78437b15567c98da4
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-02-07 17:14:01 -0300

    Created some tests using Dart Test API.

--- module-1/imc/test/imc_test.dart
+++ module-1/imc/test/imc_test.dart
@@ -1,8 +1,21 @@
 import 'package:imc/imc.dart';
-import 'package:test/test.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('Testes de calculo IMC', () {
+    test('Deve efetuar o calculo do IMC', () {
+      final result = calcIMC(1.78, 64);
+
+      expect(result, equals(20.199469763918696));
+    });
+
+    test('Deve lançar uma excessão se altura for menor que 1', () {
+      expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
+    });
+
+    test('Deve lançar uma excessão se o peso for menor que 1', () {
+      expect(() => calcIMC(1.78, 0), throwsA(isA<Exception>()));
+    });
   });
 }
